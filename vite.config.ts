d0609c7c6259b78e0e9ccc68f6159484abd8b4d6
Javier <javier@penteon.io>
2026-05-17 12:30:49 -0600

    feat: change repo root base

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   base: "/JETREJO/",
+//   plugins: [react()],
+// })
+
 export default defineConfig({
-  base: "/JETREJO/",
+  base: "/",
   plugins: [react()],
-})
+});
